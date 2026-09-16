bc2b23c0100b49d312ae0555a847901e41aeb6af
adefemgit <femijnr4@icloud.com>
2026-09-16 07:17:59 +0200

    fatory with database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 db = SQLAlchemy()
 
 
@@ -8,8 +10,11 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://jnr@localhost:5432/taskdb"
 
+    db.init_app(app)
+
     with app.app_context():
         from app import models
+        from app.routes import bp
+        app.register_blueprint(bp)
 
-    db.init_app(app)
     return app
